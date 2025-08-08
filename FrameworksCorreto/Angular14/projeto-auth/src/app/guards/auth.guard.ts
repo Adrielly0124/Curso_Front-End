6af4106945c9e9760365831882018ef93d1bc824
Adrielly0124 <drika.dantas24@gmail.com>
2025-08-08 08:20:15 -0300

    08/08/2025

--- FrameworksCorreto/Angular14/projeto-auth/src/app/guards/auth.guard.ts
+++ FrameworksCorreto/Angular14/projeto-auth/src/app/guards/auth.guard.ts
@@ -1,5 +1,20 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { CanActivate, Router } from "@angular/router";
+import { AuthService } from "../services/auth.service";
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({providedIn: 'root'})
+
+export class AuthGuard implements CanActivate{
+
+  constructor(private authService: AuthService, private router: Router){}
+
+  canActivate(): boolean{
+    if(this.authService.estaAutenticado()){
+      return true; //se o usuário estiver autenticado, permite o acesso à rota
+    } else {
+        this.router.navigate(["/login"]);
+        return false; //se não estiver autenticado, redireciona para a página de login e bloqueia o acesso à rota
+    }
+  }
+}
+
--- FrameworksCorreto/Angular14/projeto-auth/src/app/services/auth.service.ts
+++ FrameworksCorreto/Angular14/projeto-auth/src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { catchError, map, Observable, switchMap, tap, throwError } from 'rxjs';
 
 @Injectable({
@@ -14,11 +14,14 @@ export class AuthService {
   constructor(private router: Router, private http: HttpClient) { }
 
   registrar(usuario:any):Observable<any>{
+    // primeiro busca no banco de dados se o usuário já existe a partir do em
     return this.http.get<any[]>(`${this.apiUrl}?email=${usuario.email}`).pipe(
       switchMap(usuarios => {
-        if(usuarios.length>0){
-          return throwError (()=> new Error("Usuário Já Cadastrado"));
-        }else{
+        if(usuarios.length>0){ //caso exista
+          //cria uma mensagem de erro para ser tratada no try/catch
+          return throwError (()=> new Error("Usuário Já Cadastrado")); // throwError é usado para criar uma mensagem de erro que será tratada no componente catch 
+        }else{ //caso não exista
+          //cadastra o usuário no Banco de Dados
           return this.http.post<any>(this.apiUrl, usuario);
         }
       })
@@ -26,13 +29,19 @@ export class AuthService {
   }
 
   login(credenciais: any): Observable<boolean>{
+    // pega as credenciais do usuário (email e senha)
     return this.http.get<any[]>(
+      //verifica no Banco de Dados se email e senha foram encontradas (estão corretas)
       `${this.apiUrl}?email=${credenciais.email}&senha=${credenciais.senha}`).pipe(
         map(usuarios => {
-          if(usuarios.length>0){
+          if(usuarios.length>0){ //se foi encontrado
+            // armazena as informações do usuário e a chave no localStorage
             localStorage.setItem(this.CHAVE_AUTH, JSON.stringify(usuarios[0]));
+            //retorna que o acesso foi permitido
             return true;
-          }else{
+          }else{ //caso não encontrado
+            //fazer um erro
+            //retorno que meu usuário não foi encontrado e o acesso não está permitido
             //fazer um erro
             return false;
           }
@@ -40,5 +49,24 @@ export class AuthService {
       )
   }
 
-  logout(){}
+  logout(){
+    localStorage.removeItem(this.CHAVE_AUTH); //remove a chave de autenticação do usuário
+    this.router.navigate(['/home']); //redireciona para a página inicial/home
+  }
+
+  //verificar se o usuário já está logado (CHAVE_AUTH)
+  estaAutenticado(): boolean {
+    //vou transformar uma variavel di Tipo Texto em Boolean
+    return !!localStorage.getItem(this.CHAVE_AUTH); //se existir a chave retorna true, se não existir retorna false
+  }
+
+  // pegar as informações do usuário no localStorage
+  getUsuarioAtual(): any{
+    return JSON.parse(localStorage.getItem(this.CHAVE_AUTH) || '{}'); //retorna o usuário atual ou um objeto vazio
+  }
+}
+
+@Injectable({ providedIn: 'root' })
+
+export class AuthGuard implements CanActivate {
 }
